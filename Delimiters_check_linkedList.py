4ec7b04a2568f2357847bd9caab0c2a78e6e0131
Jerry Agbesi <jerryeagbesi@gmail.com>
2021-07-27 15:58:15 -0700

    final phase

--- Delimiters_check_linkedList.py
+++ Delimiters_check_linkedList.py
@@ -1,12 +1,12 @@
-
-#Creation of the stack class using a linked list
+# Creation of the stack class using a linked list
 class Node:
 
     # Class to create nodes of linked list
     # constructor initializes node automatically
-    def __init__(self,text):
+    def __init__(self, text):
         self.text = text
         self.next = None
+
 
 class Stack:
 
@@ -36,7 +36,7 @@ class Stack:
     # Remove element that is the current head (start of the stack)
     def pop(self):
 
-        #returns None if the stack is empty
+        # returns None if the stack is empty
         if self.is_empty():
             return None
 
@@ -69,32 +69,30 @@ class Stack:
                 top_node = top_node.next
         return ''
 
+
 # A separate class to check for the parenthesis
 class checker:
     def __init__(self):
         self.new_stack = Stack()
-        self.is_error = False #Returns True if an error is found and False otherwise
+        self.is_error = False  # Returns True if an error is found and False otherwise
         self.ever_popped = False
-        self.j = 0 # stores last position of an unclosed opening delimiter
+        self.j = 0  # stores last position of an unclosed opening delimiter
 
+    # Method to check for opening brackets,push them to the stack and report errors
+    def check_brackets(self, text):
 
-
-
-    #Method to check for opening brackets,push them to the stack and report errors
-    def check_brackets(self,text):
-
-        #Iterating through every character in the input given
-        for i in range(2,len(text)):
-            #storing each character of the text given by the user
+        # Iterating through every character in the input given
+        for i in range(len(text)):
+            # storing each character of the text given by the user
             current = text[i]
 
-            #checking if the current character is an opening delimiter
+            # checking if the current character is an opening delimiter
             if current in ['[', '(', '{', '<']:
                 self.j = i
                 # Pushing all opening delimiters found to a stack
                 self.new_stack.push(current)
                 # printing out the condition of the stack every time we push to it
-                print(self.new_stack.show_stack())
+                #print(self.new_stack.show_stack())
                 # checking if the current character is a closing delimiter
             elif current in [']', ')', '}', '>']:
                 # checking if the stack of opening delimiters is not empty
@@ -117,7 +115,7 @@ class checker:
                         # popping from the stack when a matching opening delimiter is found for the current closing delimiter,
                         self.new_stack.pop()
                         # showing the condition of the stack every time an item is popped from it
-                        print(self.new_stack.show_stack())
+                        # print(self.new_stack.show_stack())
                         # moving to other characters in the text
                         continue
 
@@ -125,6 +123,7 @@ class checker:
                 elif self.new_stack.is_empty():
                     # A closing delimiter without an opening leads to an error
                     self.is_error = True
+
                     print(" Error: Expected opening delimiter for  " + current + " at position " + (str(i + 1)))
                     break
                 # If no delimiter is found, no action is carried on any other character
@@ -134,25 +133,22 @@ class checker:
                 # When the sentence has balanced parenthesis
         if not self.is_error and (self.new_stack.is_empty()) and self.ever_popped:
             print("Balanced pair(s) of parenthesis,good to go")
-                # When the sentence has no parenthesis at all
+            # When the sentence has no parenthesis at all
         elif not self.is_error and (self.new_stack.is_empty()) and not self.ever_popped:
             print("No error found")
-                # when an opening delimiter is found without a closing one
+            # when an opening delimiter is found without a closing one
         elif not self.new_stack.is_empty() and not self.is_error:
             print('Error: Expected closing delimiter for ' + str(self.new_stack.peek()) + ' at position ' + str(self.j + 1))
-# text.find(self.new_stack.peek())
 
-            
-#Main program
+
+# Main program
 if __name__ == "__main__":
-    #Taking input until an empty line is met
+    # Taking input until an empty line is met
     while True:
         Input_text = input("********************************\nenter a sentence with delimiters: \n  ")
         if not Input_text:
             break
-        #creating an instance for the checker class
+        # creating an instance for the checker class
         checker1 = checker()
-        #calling the method to check brackets
+        # calling the method to check brackets
         checker1.check_brackets(Input_text)
-        
-    #Remember to keep track of the number of left parenthesis   
